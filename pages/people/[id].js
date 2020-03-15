95a45a437814b86f3ca9def906c6141efb0e3874
Allan Roscoche <allanroscoche@gmail.com>
2020-03-15 20:37:56 -0300

    added page structure

---
+++ pages/people/[id].js
@@ -0,0 +1,10 @@
+import { useRouter } from 'next/router';
+
+export default () => {
+    const router = useRouter();
+    const { id } = router.query
+
+    return (
+        <p>Id: {id} </p>
+    );
+}
